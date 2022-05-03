f24610553fb8ed8fa4bc7b1d2ce236de84f91e17
aevilesaguiar <aeviles@hotmail.com>
2022-05-03 14:26:04 -0300

    Initial commit

---
+++ src/main/java/com/dio/santader/bankline/api/ApiRestDoProjetoBancklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santader.bankline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDoProjetoBancklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDoProjetoBancklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santader/bankline/api/ApiRestDoProjetoBancklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santader.bankline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDoProjetoBancklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
